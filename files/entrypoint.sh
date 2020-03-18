9b22a8f559638c47eb7a53ae79065b4909b42b4b
devopskit <devopskit@gmail.com>
2020-03-18 21:52:19 +0100

    Added README. Added a check for resolv.conf in entrypoint.

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -8,6 +8,8 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 export LC_ALL LANG PATH
 
 declare -r DNSMASQ_CONFIG=/data/dnsmasq.conf
+declare -r RESOLV_CONFIG=/data/resolv.conf
+
 
 # if volume with config, use it
 if [ -f "${DNSMASQ_CONFIG}" ];
@@ -15,6 +17,11 @@ then \
     rm -f /etc/dnsmasq.conf
 	ln -s "${DNSMASQ_CONFIG}" /etc/dnsmasq.conf
 fi
-# else if env vars use them
+
+if [ -f "${RESOLV_CONFIG}" ];
+then \
+    rm -f /etc/dnsmasq.conf
+	ln -s "${RESOLV_CONFIG}" /etc/dnsmasq.conf
+fi
 
 exec dnsmasq $@ -n -d -C /etc/dnsmasq.conf 
